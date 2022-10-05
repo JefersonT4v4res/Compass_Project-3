c2c2e1b105c92f9b82678de214375775bbfd2a48
JefersonT4v4res <jefersonsaj_tavares@hotmail.com>
2022-10-05 01:33:12 -0300

    Adicionando cards na toDoList, salvando informações

--- Projeto - Sprint 3 - Planejamento Semana/assets/js/addEvent.js
+++ Projeto - Sprint 3 - Planejamento Semana/assets/js/addEvent.js
@@ -1,44 +1,45 @@
 let btn_add = document.querySelector(".add");
 
-var activity = [
-    {
-        tab_day: "monday",
-        day_week: "segunda-feira",
-        hour_activity: "13h00m",
-        event_description: "Mussum ipsum",
-        conflict: "false"
-    }
-]
-
-console.log(activity[0].day_week);
-
-function createCard() {
+var activity = [ ];
     
-    for(let i = 0; i <= activity.length; i++) {
-        let tab = document.querySelector(".bnt__days");
-        let tab_class = "board-" + tab.id;
-        document.querySelector("."+tab_class).insertAdjacentHTML("afterbegin", "<div class='toDoList__board-card'>" + "<div class='card__hour'>" + activity[0].hour_activity +"</div>" +
-        "<div class='card__content'>" + "<p class='card__content-activity'>" + "<button class='card__content-btn_erase'>Apagar</button>");
-       
+function focusOnTab(tab, board) {
+    //document.querySelector(tab).classList.toggle('bnt__days-active');
+   // document.querySelector(board).style.display = "flex";
+}
+
+function getDayWeek() {
+    let id, tab;
+    let span_day = document.querySelector(".chosen-day").innerHTML;
+    let days = document.querySelectorAll(".option-day");
+
+    for(let i=0; i <= days.length; i++) {
+        if(days[i].textContent == span_day) {
+            id = ".board-" + days[i].id;
+            tab = ".btn-" + days[i].id;
+            focusOnTab(tab, id);
+            return id;
+        }
     }
 }
 
+function createCard() {
+    var i = activity.length - 1;
+    let tab = getDayWeek();
+ 
+     document.querySelector(tab).insertAdjacentHTML("afterbegin", "<div class='toDoList__board-card' id='"+ i +"'>" + "<div class='card__hour'>" + activity[i].hour_activity +"</div>" +
+     "<div class='card__content'>" + "<p class='card__content-activity'>" + activity[i].event_description + "</p><button class='card__content-btn_erase'>Apagar</button>");
+}
+
 function saveInformations() {
-    let conflict_hour = document.querySelector(".input__hour").value;
-    let conflict_day = document.querySelector(".chosen-day").innerHTML;
-
-    console.log(conflict_hour + " " + " " + conflict_day );
-
-   
-    activity[i].day_week = document.querySelector(".chosen-day").innerHTML;
-    activity[i].hour_activity = document.querySelector(".input__hour").value;
-    activity[i].event_description = document.querySelector(".event-description").value;
-    activity[i].conflict = activity[i];
+     
+    activity.push({'day_week': document.querySelector(".chosen-day").innerHTML,
+    'hour_activity': document.querySelector(".input__hour").value,
+    'event_description': document.querySelector(".event-description").value});
 
     createCard();
 }
 
-function inptEmpty(){
+function inptEmpty() {
    
     if(document.querySelector(".event-description").value == "") {
         document.querySelector(".event-description").style.border = "0.0625rem solid red";
--- Projeto - Sprint 3 - Planejamento Semana/assets/js/script.js
+++ Projeto - Sprint 3 - Planejamento Semana/assets/js/script.js
@@ -25,24 +25,48 @@ dropH.addEventListener("click", dropdownH)
 
 function removeClass(className, tab_class) {
     flag++;
+ 
     if(flag == 2){
         document.querySelector(lastDay).classList.toggle('bnt__days-active');
         document.querySelector(lastTab).style.display = "none";
         flag--;
+        console.log(flag);
         lastDay = className;
         lastTab = tab_class;
     }else {
         lastDay = className;
         lastTab = tab_class;
+    
     }
 }
 
-function addClass(event) {
-    document.querySelector("." + event.classList[1]).classList.toggle('bnt__days-active');
-    let nameClass = ("." + event.classList[1]);
-    let tab = document.querySelector(nameClass);
-    let tab_class = ".board-" + tab.id;
-    document.querySelector(tab_class).style.display = "flex";
-    removeClass(nameClass, tab_class);
+function checkActive(event) {
+    let tab_days = document.querySelectorAll(".bnt__days");
+    let isActive;
 
+    console.log("tou aqui");
+    for(let i=0; i <= tab_days.length; i++ ) {
+        isActive = tab_days[i].classList.contains(".bnt__days-active");
+            if(isActive) {
+                console.log("Passei if");
+                /*let dayClass = getActive.classList[1];
+                let tabclass = ".board-" + event.id;
+                document.querySelector("."+dayClass).classList.toggle('.bnt__days-active');*/
+            }
+    }       
+}
+
+function addClass(event) {
+//checkActive(event);
+        document.querySelector("." + event.classList[1]).classList.toggle('bnt__days-active');
+        let nameClass = ("." + event.classList[1]);
+        console.log(nameClass);
+        let tab = document.querySelector(nameClass);
+        console.log(tab);
+        let tab_class = ".board-" + tab.id;
+        console.log(tab_class);
+        document.querySelector(tab_class).style.display = "flex";
+        removeClass(nameClass, tab_class);   
+
+        
 }
